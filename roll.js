0a5d8018a7f18f86ebeac548ee9fdeb93f042aa7
unknown <maximemarand@gmail.com>
2021-01-19 16:24:53 +0100

    second commit roll.js

--- roll.js
+++ roll.js
@@ -7,38 +7,36 @@
 
 //Le premier joueur qui atteint 100 POINTS sur le global score gagne le jeu;
 
-let currentscore1 = 0;
-let currentscore2 = 0;
+
 
 let rollresult = document.querySelector('.rollresult')
 let player1 = document.querySelector('.p1')
 let player2 = document.querySelector('.p2')
+let icon4 = document.querySelector('.icon4')
+let icon5 = document.querySelector('.icon5')
+let currents1 = document.querySelector('.displaycurrentscore1')
 
-const rolldice = document.querySelector('span')
+
+const rolldice = document.querySelector('.rolldice')
 rolldice.addEventListener('click', displayRollDice)
 
-//Résultat d'un lancé ajouté au current score;
 let player1Turn = true;
 let player2Turn = false;
 
+let currentscore1 = 0
+//Résultat d'un lancé ajouté au current score;
+
 function displayRollDice() {
   let roll = Math.floor(Math.random()*6)+1;
+  currentscore1 = currentscore1 + roll
+  currents1.innerHTML = `${currentscore1}`
+  currents1.style.fontSize = "50px"
+  currents1.style.color = "white"
   rollresult.innerHTML = `${roll}`
   rollresult.style.fontSize = "100px"
   
-  if(player1Turn) {
-    player1.style.fontWeight = "normal"
-  } else {
-    player1.style.fontWeight = "lighter"
-  }
-  
-  if(player2Turn) {
-    player2.style.fontWeight = "normal"
-  } else {
-    player2.style.fontWeight = "lighter"
-  }
-
-  if(player1Turn && roll === 1){
+  //définition du tour de jeu
+ if(player1Turn && roll === 1){
     player1Turn = false
     if(player1Turn === false){
       player2Turn = true
@@ -51,6 +49,19 @@ function displayRollDice() {
   } else if(player1Turn){
     player2Turn = false
   } 
+  
+  if(player1Turn) {
+    player1.style.fontWeight = "normal"
+  } else {
+    player1.style.fontWeight = "lighter"
+  }
+  
+  if(player2Turn) {
+    player2.style.fontWeight = "normal"
+  } else {
+    player2.style.fontWeight = "lighter"
+  }
+  
 }
 
 
